97b0cee20423aae6afe3b4e1d802d43812baa276
Rahul Puri <purirahul884@gmail.com>
2026-01-08 16:50:46 +0530

    added program for char count in string

---
+++ CountChar.java
@@ -0,0 +1,35 @@
+class CountChar
+{
+	public static void main(String[] args)
+	{
+		String a ="harsh";
+		String b="";
+		int s=a.length();
+		System.out.println(s);
+		for(int i=0; i<s; i++)
+		{
+			int count =0;
+				
+			if(!(b.contains(String.valueOf(a.charAt(i)))))
+			{
+					
+				for(int j=0; j<s; j++)
+				{
+					if(a.charAt(i)==a.charAt(j))
+					{
+						count++;
+						
+						
+					}
+				}
+				System.out.println(a.charAt(i) +"="+ count);
+				b=b+a.charAt(i);
+			}
+			
+			
+			
+		}
+		
+		System.out.println(b);			
+	}
+}
